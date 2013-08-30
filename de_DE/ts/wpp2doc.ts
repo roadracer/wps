cbcc21260f31cd938f042d84a2fd48da214df0a7
zer0-de <github@altjuehrden.de>
2013-08-30 13:00:29 +0200

    all rows translated

--- de_DE/ts/wpp2doc.ts
+++ de_DE/ts/wpp2doc.ts
@@ -4,231 +4,232 @@
 <context>
     <name>KTranslator</name>
     <message>
-        <source>Slide %1
-</source>
-        <translation type="unfinished"></translation>
+        <source>Slide %1</source>
+        <translation>Schieben %1</translation>
     </message>
     <message>
         <source>Notes Page:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hinweisseite:</translation>
     </message>
     <message>
         <source>Being converted</source>
-        <translation type="unfinished"></translation>
+        <translation>wird konvertiert</translation>
     </message>
     <message>
         <source>Still Need:</source>
-        <translation type="unfinished"></translation>
+        <translation>Wird benötigt:</translation>
     </message>
     <message>
         <source> Minute</source>
-        <translation type="unfinished"></translation>
+        <translation> Minute</translation>
     </message>
     <message>
         <source>Second</source>
-        <translation type="unfinished"></translation>
+        <translation>Sekunde</translation>
     </message>
     <message>
         <source>Ready to convert, please wait</source>
-        <translation type="unfinished"></translation>
+        <translation>Fertig zum konvertieren, bitte warten</translation>
     </message>
     <message>
         <source>Being calculated</source>
-        <translation type="unfinished"></translation>
+        <translation>wird berechnet</translation>
     </message>
     <message>
         <source>Start converting</source>
-        <translation type="unfinished"></translation>
+        <translation>Starte Konvertierung</translation>
     </message>
 </context>
 <context>
     <name>ProcessDialog</name>
     <message>
         <source>Translate to Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokument übersetzen</translation>
     </message>
     <message>
         <source>Ready to convert</source>
-        <translation type="unfinished"></translation>
+        <translation>Fertig zum konvertieren</translation>
     </message>
     <message>
         <source>Still Need:Being calculated</source>
-        <translation type="unfinished"></translation>
+        <translation>Wird benötigt: wird berechnet</translation>
     </message>
     <message>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei öffnen</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
 </context>
 <context>
     <name>changeAsWpsDialog</name>
     <message>
         <source>Translate to Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokument übersetzen</translation>
     </message>
     <message>
         <source>All(&amp;A)</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle(&amp;A)</translation>
     </message>
     <message>
         <source>Selection(&amp;S)</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswahl(&amp;S)</translation>
     </message>
     <message>
         <source>Slides(&amp;P):</source>
-        <translation type="unfinished"></translation>
+        <translation>Slides(&amp;P):</translation>
     </message>
     <message>
         <source>According to original layout(&amp;O)</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Original-Layout(&amp;O)</translation>
     </message>
     <message>
         <source>Add notes to slide:</source>
-        <translation type="unfinished"></translation>
+        <translation>Anmerkungen zur Folie hinzufügen:</translation>
     </message>
     <message>
         <source>Right(&amp;R)</source>
-        <translation type="unfinished"></translation>
+        <translation>Rechts(&amp;R)</translation>
     </message>
     <message>
         <source>Below(&amp;B)</source>
-        <translation type="unfinished"></translation>
+        <translation>Unterhalb(&amp;B)</translation>
     </message>
     <message>
         <source>Below(&amp;N)</source>
-        <translation type="unfinished"></translation>
+        <translation>Unterhalb(&amp;B)</translation>
     </message>
     <message>
         <source>Right(&amp;H)</source>
-        <translation type="unfinished"></translation>
+        <translation>Rechts(&amp;H)</translation>
     </message>
     <message>
         <source>Add blank line to slide:</source>
-        <translation type="unfinished"></translation>
+        <translation>Füge durchsichtige Linie zur Folie hinzu:</translation>
     </message>
     <message>
         <source>Slide per page(&amp;E):</source>
-        <translation type="unfinished"></translation>
+        <translation>Folien pro Seite(&amp;E):</translation>
     </message>
     <message>
         <source>1</source>
-        <translation type="unfinished"></translation>
+        <translation>1</translation>
     </message>
     <message>
         <source>2</source>
-        <translation type="unfinished"></translation>
+        <translation>2</translation>
     </message>
     <message>
         <source>3</source>
-        <translation type="unfinished"></translation>
+        <translation>3</translation>
     </message>
     <message>
         <source>4</source>
-        <translation type="unfinished"></translation>
+        <translation>4</translation>
     </message>
     <message>
         <source>Number slides in the document</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzahl Folien im Dokument</translation>
     </message>
     <message>
         <source>Preview</source>
-        <translation type="unfinished"></translation>
+        <translation>Vorschau</translation>
     </message>
     <message>
         <source>Text(&amp;T)</source>
-        <translation type="unfinished"></translation>
+        <translation>Text(&amp;T)</translation>
     </message>
     <message>
         <source>Table(&amp;L)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tabelle(&amp;L)</translation>
     </message>
     <message>
         <source>Picture(&amp;I)</source>
-        <translation type="unfinished"></translation>
+        <translation>Bild(&amp;I)</translation>
     </message>
     <message>
         <source>Select Slide</source>
-        <translation type="unfinished"></translation>
+        <translation>Folie wählen</translation>
     </message>
     <message>
         <source>Current Slide(&amp;C)</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuell Folie(&amp;C)</translation>
     </message>
     <message>
         <source>Converted Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>konvertiertes Layout</translation>
     </message>
     <message>
         <source>Conversion Includes</source>
-        <translation type="unfinished"></translation>
+        <translation>Konvertierungsbestandteile</translation>
     </message>
 </context>
 <context>
     <name>kxProgressdlg</name>
     <message>
         <source>The conversion is completed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Konvertierung ist abgeschlossen.</translation>
     </message>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Schließen</translation>
     </message>
     <message>
         <source>Converting to Kingsoft Writer document encountered an error.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bei der Konvertierung zu Kingsoft Writer Dokument ist ein Fehler aufgetreten.</translation>
     </message>
 </context>
 <context>
     <name>kxwpptodocdlg</name>
     <message>
         <source>Input slide number or slide range. For example, 1,3,5-9</source>
-        <translation type="unfinished"></translation>
+        <translation>Foliennummer oder Folienbereich eingeben. Zum Beispiel, 1,3,5-9</translation>
     </message>
     <message>
         <source>The number must be 1, 2, 3, or 4.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Nummer muss 1,2,3 oder 4 sein.</translation>
     </message>
     <message>
         <source>This is a valid number.
 Please input slide number or slide range. 
 For example, 1,3,5-9.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dies ist keine gültige Nummer.
+Bitte geben Sie eine Foliennummer oder einen Folienebereich ein. 
+Zum Beispiel, 1,3,5-9.</translation>
     </message>
     <message>
         <source>Kingsoft Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Kingsoft Präsentation</translation>
     </message>
     <message>
         <source>The number must be 1 or 2.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Nummer muss 1 oder 2 sein</translation>
     </message>
     <message>
         <source>Kingsoft Writer Document(*.wps)</source>
-        <translation type="unfinished"></translation>
+        <translation>Kingsoft Writer Dokument(*.wps)</translation>
     </message>
     <message>
         <source>Microsoft Word 97/2000/XP/2003 Document(*.doc)</source>
-        <translation type="unfinished"></translation>
+        <translation>Microsoft Word 97/2000/XP/2003 Dokument(*.doc)</translation>
     </message>
     <message>
         <source>Save As</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichern unter</translation>
     </message>
     <message>
         <source>The file &quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Datei &quot;</translation>
     </message>
     <message>
         <source>&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;</translation>
     </message>
     <message>
         <source>already exists. You cannot save the file with the same name as another open file. Choose a different name, or close the open file before saving.</source>
-        <translation type="unfinished"></translation>
+        <translation>Existiert bereits. Sie können die Datei nicht unter gleichem Namen speichern, solange eine Datei mit dem selben Namen geöffnet ist. Wählen Sie einen anderen Namen oder schließen die geöffnete Datei.</translation>
     </message>
 </context>
 </TS>
