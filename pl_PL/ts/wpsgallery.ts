77c19c9784aac92913a4b0d60ff7dd13a49e68f5
Astraltrooper <kujk6a@o2.pl>
2013-12-04 08:08:32 +0100

    Update wpsgallery.ts
    translation need correction

--- pl_PL/ts/wpsgallery.ts
+++ pl_PL/ts/wpsgallery.ts
@@ -5,26 +5,26 @@
     <name>KGalleryClient</name>
     <message>
         <source>gallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">galeria</translation>
     </message>
     <message>
         <source>shape</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">shape</translation>
     </message>
 </context>
 <context>
     <name>KxDlgEditGallery</name>
     <message>
         <source>EditGallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Edytuj Galerię</translation>
     </message>
     <message>
         <source>From:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Od:</translation>
     </message>
     <message>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tekst</translation>
     </message>
     <message>
         <source>Caption:</source>
@@ -32,7 +32,7 @@
     </message>
     <message>
         <source>Label:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Etykieta:</translation>
     </message>
     <message>
         <source>The Most Number Of Caption Is 5.</source>
@@ -40,56 +40,56 @@
     </message>
     <message>
         <source>More:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Więcej:</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">OK</translation>
     </message>
     <message>
         <source>Directory:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Katalog:</translation>
     </message>
     <message>
         <source>Notes:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Notatki:</translation>
     </message>
     <message>
         <source>Less:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Mniej:</translation>
     </message>
 </context>
 <context>
     <name>KxEditGalleryDlg</name>
     <message>
         <source>More</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Więcej</translation>
     </message>
     <message>
         <source>Less</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Mniej</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
         <source>\Kingsoft Office\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\Desktop.ini name.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\AppData\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\AppData\Desktop.ini name.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\AppData\My Materials\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\AppData\My Materials\Desktop.ini name.</translation>
     </message>
 </context>
 <context>
     <name>WPSGalleryClass</name>
     <message>
         <source>WPSGallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">WPS Galeria</translation>
     </message>
 </context>
 </TS>
