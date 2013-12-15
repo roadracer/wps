fead4072bd902dcfc8f1689fdc69903719e01d44
armstrong1992 <armstrong.smok+github@gmail.com>
2013-12-15 15:10:45 +0100

    Poprawa tłumaczeń

--- pl_PL/ts/wpscore.ts
+++ pl_PL/ts/wpscore.ts
@@ -51,11 +51,11 @@
     </message>
     <message>
         <source>Error! The style name is not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd! Nazwa stylu niezdefiniowana.</translation>
     </message>
     <message>
         <source>Error! The style is not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd! Styl nie jest ustawiony.</translation>
     </message>
     <message>
         <source>Error!</source>
@@ -70,7 +70,7 @@
     </message>
     <message>
         <source> </source>
-        <translation type="unfinished"></translation>
+        <translation> </translation>
     </message>
     <message>
         <source>Error! Bookmark not defined.</source>
@@ -105,7 +105,7 @@
     <name>field_parser_links_and_reference</name>
     <message>
         <source>Error! Hyperlink reference is invalid.</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd! Odnośnik hiperłącza jest nieprawidłowy.</translation>
     </message>
     <message>
         <source>Hyperlink</source>
@@ -141,11 +141,11 @@
     </message>
     <message>
         <source>Error! The file you specified cannot be open.</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd! Plik który wybrałeś nie może zostać otwarty.</translation>
     </message>
     <message>
         <source>Error! Filename not specified.</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd! Nazwa pliku niezdefiniowana.</translation>
     </message>
     <message>
         <source>Error! Title list level is invalid.</source>
@@ -248,7 +248,7 @@
     <name>renderhost</name>
     <message>
         <source>Page Break</source>
-        <translation>Pdoział Strony</translation>
+        <translation>Podział Strony</translation>
     </message>
     <message>
         <source>Column Break</source>
@@ -2162,7 +2162,7 @@ To continue saving as a macro-free workbook, click Yes.</source>
     </message>
     <message>
         <source>The file already exists. Do you want to replace the existing file?</source>
-        <translation type="unfinished"></translation>
+        <translation>Plik istnieje. Czy chcesz go zastąpić?</translation>
     </message>
     <message>
         <source>&amp;Find All</source>
@@ -3242,28 +3242,50 @@ To continue saving as a macro-free workbook, click Yes.</source>
     </message>
     <message>
         <source>Do you want to change the default settings for page setup?
-This change will affect all new documents based on the %s template.</source>
-        <translation type="unfinished"></translation>
+
+This change will affect all new documents based on the %s template.</source>
+        <translation>Czy chcesz zmienić domyślne ustawienia strony?
+
+Ta zmiana będzie mieć wpływ na wszystkie nowe dokumenty utworzone na podstawie szablonu %s.</translation>
     </message>
     <message>
         <source>Do you want to change the default settings for font?
-This change will affect all the new documents based on the %s template.</source>
-        <translation type="unfinished"></translation>
+
+This change will affect all the new documents based on the %s template.</source>
+        <translation>Czy chcesz zmienić domyślne ustawienia dla czcionki?
+
+Ta zmiana będzie mieć wpływ na wszystkie nowe dokumenty utworzone na podstawie szablonu %s.</translation>
     </message>
     <message>
         <source>Do you want to change the default settings for style?
-This change will affect all the new documents based on the %s template.
 
-Yes:	Save to template;
-No:	Do not save to template;
-Cancel:	Return to the dialog.</source>
-        <translation type="unfinished"></translation>
+This change will affect all the new documents based on the %s template.
+
+
+Yes:	Save to template;
+
+No:	Do not save to template;
+
+Cancel:	Return to the dialog.</source>
+        <translation>Czy chcesz zmienić domyślne ustawienia dla stylu?
+
+Ta zmiana będzie mieć wpływ na wszystkie nowe dokumenty utworzone na podstawie szablonu %s.
+
+
+Tak:	Zapisz do szablonu;
+
+Nie:	Nie zapisuj do szablonu;
+
+Anuluj:	Wróć do okna dialogowego.
+</translation>
     </message>
     <message>
         <source>Heading1...........................1
   Heading2.........................3
     Heading3.......................5</source>
-        <translation type="unfinished"></translation>
+        <translation>Nagłówek1...........................1
+  Nagłówek2.........................3
+    Nagłówek3.......................5</translation>
     </message>
     <message>
         <source>(none)
@@ -3363,7 +3385,7 @@ To continue saving as a macro-free workbook, click Yes.</source>
     </message>
     <message>
         <source>Select Data Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz Źródło Danych</translation>
     </message>
     <message>
         <source>New Blank Document</source>
