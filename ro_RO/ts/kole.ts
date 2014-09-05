ba408f85961846526feb71a33ba7ba5d92a21424
masac <daviodan@yahoo.com>
2014-09-05 10:39:52 +0300

    added romanian language

---
+++ ro_RO/ts/kole.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ro_RO" sourcelanguage="ro">
+<context>
+    <name>KAxServer</name>
+    <message>
+        <source>Do you want to save changes to the file?</source>
+        <translation>Vrei să salvezi modificările într-un fişier?</translation>
+    </message>
+</context>
+</TS>
