72224bf5e61fcad89a403292441234126b8411c8
Hakan <hakanseven12@gmail.com>
2016-03-04 11:07:18 +0200

    Update kde.ts

--- tr_TR/ts/kde.ts
+++ tr_TR/ts/kde.ts
@@ -7,7 +7,7 @@
     <message>
         <source>All Reference Files</source>
         <extracomment>Unknown position</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tüm Kaynak Dosyaları</translation>
     </message>
 </context>
 <context>
@@ -15,32 +15,32 @@
     <message>
         <source>&amp;Description:</source>
         <extracomment>et -&gt; Developer -&gt; Macros -&gt; Options</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Açıklama:</translation>
     </message>
     <message>
         <source>Macro Options</source>
         <extracomment>et -&gt; Developer -&gt; Macros -&gt; Options</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Makro Seçenekleri</translation>
     </message>
     <message>
         <source>Macro name:</source>
         <extracomment>et -&gt; Developer -&gt; Macros -&gt; Options</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Macro ismi:</translation>
     </message>
     <message>
         <source>Prompt</source>
         <extracomment>et -&gt; developer -&gt; Macros -&gt; chooice a macro -&gt; Options -&gt; Shortcut key: = -&gt; OK</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>İstem</translation>
     </message>
     <message>
         <source>Shortcut &amp;key:</source>
         <extracomment>et -&gt; Developer -&gt; Macros -&gt; Options</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kısayol &amp;tuşu:</translation>
     </message>
     <message>
         <source>Shortcut key must be a letter.</source>
         <extracomment>et -&gt; developer -&gt; Macros -&gt; chooice a macro -&gt; Options -&gt; Shortcut key: = -&gt; OK</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kısayol tuşu bir harf olmalıdır.</translation>
     </message>
 </context>
 <context>
@@ -48,7 +48,7 @@
     <message>
         <source>&amp;Create</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Oluştur</translation>
     </message>
     <message>
         <source>&amp;Delete</source>
@@ -63,12 +63,12 @@
     <message>
         <source>&amp;Macro name:</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Makro ismi:</translation>
     </message>
     <message>
         <source>&amp;Options</source>
         <extracomment>et -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Seçenekler</translation>
     </message>
     <message>
         <source>&amp;Run</source>
@@ -78,42 +78,42 @@
     <message>
         <source>&amp;Step Into</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Uyarla</translation>
     </message>
     <message>
         <source>All active templates and documents</source>
         <extracomment>developer -&gt; Macro in:</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tüm aktif şablonlar ve belgeler</translation>
     </message>
     <message>
         <source>Cancel</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation>İptal</translation>
+        <translation>Vazgeç</translation>
     </message>
     <message>
         <source>Descript&amp;ion:</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Açıklam&amp;a:</translation>
     </message>
     <message>
         <source>Do you want to delete macro %1 ?</source>
         <extracomment>developer -&gt; Macros -&gt; delete</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>%1 makro silinsin mi?</translation>
     </message>
     <message>
         <source>Invalid procedure name.</source>
         <extracomment>developer -&gt; Macros -&gt; Macro in:(choose 2nd) -&gt; Macro name:( xxx, ) -&gt; Create</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Geçersiz nitel bölüm ismi.</translation>
     </message>
     <message>
         <source>M&amp;acro in:</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>M&amp;akrodaki:</translation>
     </message>
     <message>
         <source>Macros</source>
         <extracomment>wps -&gt; Developer -&gt; Macros</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Makrolar</translation>
     </message>
     <message>
         <source>Prompt</source>
@@ -124,17 +124,17 @@ developer -&gt; Macros -&gt; delete
 Theoretically, will not displayed any where.
 ----------
 developer -&gt; Macros -&gt; Macro in:(choose 2nd) -&gt; Macro name:( xxx, ) -&gt; Create</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>İstem</translation>
     </message>
     <message>
         <source>The macros in this project are disabled.</source>
         <extracomment>developer -&gt; Security -&gt; VeryHigh; developer -&gt; Macros -&gt; Run</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bu projedeki makrolar devre dışı bırakıldı.</translation>
     </message>
     <message>
         <source>The project item name is not valid.</source>
         <extracomment>Theoretically, will not displayed any where.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Proje öğesi adı geçersiz.</translation>
     </message>
 </context>
 <context>
@@ -142,32 +142,32 @@ developer -&gt; Macros -&gt; Macro in:(choose 2nd) -&gt; Macro name:( xxx, ) -&g
     <message>
         <source>&quot;%1&quot; contains macros.</source>
         <extracomment>wps -&gt; Security -&gt; set to medium -&gt; open a file with macro</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; makrolar içeriyor.</translation>
     </message>
     <message>
         <source>&amp;Disable Macros</source>
         <extracomment>wps -&gt; Security -&gt; set to medium -&gt; open a file with macro</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Makroları Devredışı Bırak</translation>
     </message>
     <message>
         <source>&amp;Enable Macros</source>
         <extracomment>wps -&gt; Security -&gt; set to medium -&gt; open a file with macro</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Makroları Etkinleştir</translation>
     </message>
     <message>
         <source>Macros may contain viruses. It is always safe to disable macros, but if the macros are legitimate, you might lose some functionality.</source>
         <extracomment>wps -&gt; Security -&gt; set to medium -&gt; open a file with macro</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Makrolar virüs içerebilir. Makroları devre dışı bırakmak her zaman güvenlidir, ancak makrolar yasalsa, işlevlerden bazılarını kaybedebilirsiniz.</translation>
     </message>
     <message>
         <source>Security Warning</source>
         <extracomment>wps -&gt; Security -&gt; set to medium -&gt; open a file with macro</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Güvenlik Uyarısı</translation>
     </message>
     <message>
         <source>The file you would like to open contains macros.</source>
         <extracomment>Unknown position</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Açmak istediğiniz dosya makrolar içeriyor.</translation>
     </message>
 </context>
 <context>
@@ -175,67 +175,67 @@ developer -&gt; Macros -&gt; Macro in:(choose 2nd) -&gt; Macro name:( xxx, ) -&g
     <message>
         <source>&amp;High</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Yüksek</translation>
     </message>
     <message>
         <source>&amp;Low(not recommended)</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Düşük (önerilmez)</translation>
     </message>
     <message>
         <source>&amp;Medium</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Orta</translation>
     </message>
     <message>
         <source>&amp;Remove</source>
         <extracomment>developer -&gt; Security -&gt; Trusted Publishers</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Kaldır</translation>
     </message>
     <message>
         <source>&amp;Security Level</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Güvenlik Seviyesi</translation>
     </message>
     <message>
         <source>&amp;Trusted Publishers</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Güvenilir Yayıncılar</translation>
     </message>
     <message>
         <source>&amp;Very high</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Çok yüksek</translation>
     </message>
     <message>
         <source>Only macros installed in trusted locations will be allowed to run. All other signed and unsigned macros are disabled.</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Sadece güvenilen yerlerden yüklenen makroların çalışmasına izin verilecektir. Tüm diğer imzalı ve imzasız makrolar devre dışı bırakılır.</translation>
     </message>
     <message>
         <source>Only signed macros from trusted sources will be allowed to run. Unsigned macros are automatically disabled.</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Sadece güvenilir kaynaklardan imzalı makroların çalışmasına izin verilecektir. İmzasız makrolar otomatik olarak devre dışı bırakılır.</translation>
     </message>
     <message>
         <source>Security</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Güvenlik</translation>
     </message>
     <message>
         <source>Trust access to &amp;Visual Basic Project</source>
         <extracomment>developer -&gt; Security -&gt; Trusted Publishers</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Visual Basic Projesine Güvenli Erişim</translation>
     </message>
     <message>
         <source>You are not protected from potentially unsafe macros. Use this setting only if you have virus scanning software installed, or you have checked the safety of all documents you open.</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Potansiyel güvenli olmayan makrolardan korunmuyorsunuz. Eğer virüs tarama yazılımı yüklüyse veya açtığınız tüm belgelerin güvenliğini kontrol ettiyseniz bu ayarı kullanın.</translation>
     </message>
     <message>
         <source>You can choose whether or not to run potentially unsafe macros.</source>
         <extracomment>developer -&gt; Security</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Güvenli olmayan makroları çalıştırmak isteyip istemediğinizi seçebilirsiniz.</translation>
     </message>
 </context>
 </TS>
