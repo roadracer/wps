1f1727552dc2d1d8a471a501f506bea0425eab8e
Hakan <hakanseven12@gmail.com>
2016-03-04 11:34:05 +0200

    Update kwpsassist.ts

--- tr_TR/ts/kwpsassist.ts
+++ tr_TR/ts/kwpsassist.ts
@@ -6,187 +6,188 @@
     <name>KMoreExtendGalleryTile</name>
     <message>
         <source>Browse others</source>
-        <translation type="unfinished"></translation>
+        <translation>Diğerlerine göz at</translation>
     </message>
     <message>
         <source>More templates...</source>
-        <translation type="unfinished"></translation>
+        <translation>Daha fazla şablon...</translation>
     </message>
     <message>
         <source>Standard screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Standart Ekran</translation>
     </message>
     <message>
         <source>Wide screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Geniş ekran</translation>
     </message>
 </context>
 <context>
     <name>KSlideLibraryLogic</name>
     <message>
         <source>ContentSlideType_Cover</source>
-        <translation type="unfinished"></translation>
+        <translation>ContentSlideType_Cover</translation>
     </message>
     <message>
         <source>ContentSlideType_Catalog</source>
-        <translation type="unfinished"></translation>
+        <translation>ContentSlideType_Catalog</translation>
     </message>
     <message>
         <source>ContentSlideType_Main</source>
-        <translation type="unfinished"></translation>
+        <translation>ContentSlideType_Main</translation>
     </message>
     <message>
         <source>ContentSlideType_Trans</source>
-        <translation type="unfinished"></translation>
+        <translation>ContentSlideType_Trans</translation>
     </message>
     <message>
         <source>ContentSlideType_Ending</source>
-        <translation type="unfinished"></translation>
+        <translation>ContentSlideType_Ending</translation>
     </message>
     <message>
         <source>ContentSlideType_More</source>
-        <translation type="unfinished"></translation>
+        <translation>ContentSlideType_More</translation>
     </message>
 </context>
 <context>
     <name>KTemplateDetailHeaderButton</name>
     <message>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Geri</translation>
     </message>
 </context>
 <context>
     <name>KTemplateDlgBottom</name>
     <message>
         <source>Blank document</source>
-        <translation type="unfinished"></translation>
+        <translation>Boş belge</translation>
     </message>
     <message>
         <source>From template</source>
-        <translation type="unfinished"></translation>
+        <translation>Şablondan</translation>
     </message>
 </context>
 <context>
     <name>KTemplateDlgDetailHeader</name>
     <message>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeni</translation>
     </message>
 </context>
 <context>
     <name>KTemplateDlgHeader</name>
     <message>
         <source>Standard</source>
-        <translation type="unfinished"></translation>
+        <translation>Standart</translation>
     </message>
     <message>
         <source>Wide screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Geniş ekran</translation>
     </message>
     <message>
         <source>Classification</source>
-        <translation type="unfinished"></translation>
+        <translation>Sınıflandırma</translation>
     </message>
     <message>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeni</translation>
     </message>
 </context>
 <context>
     <name>KTemplateDownloadFinishTip</name>
     <message>
         <source>apply</source>
-        <translation type="unfinished"></translation>
+        <translation>uygula</translation>
     </message>
     <message>
         <source>template download success</source>
-        <translation type="unfinished"></translation>
+        <translation>şablon indirme başarılı</translation>
     </message>
     <message>
         <source>template download failed</source>
-        <translation type="unfinished"></translation>
+        <translation>şablon indirme başarısız</translation>
     </message>
 </context>
 <context>
     <name>KTemplateExtendGallery</name>
     <message>
         <source>Recently Used</source>
-        <translation type="unfinished"></translation>
+        <translation>Son Kullanılanlar</translation>
     </message>
     <message>
         <source>More templates</source>
-        <translation type="unfinished"></translation>
+        <translation>Daha fazla şablon...</translation>
     </message>
 </context>
 <context>
     <name>KWppTemplateItem</name>
     <message>
         <source>Load failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Yükleme başarısız.</translation>
     </message>
     <message>
         <source>Apply to &amp;All Slides</source>
-        <translation type="unfinished"></translation>
+        <translation>Tüm Slayt&amp;lara Uygula</translation>
     </message>
     <message>
         <source>Apply to &amp;Selected Slides</source>
-        <translation type="unfinished"></translation>
+        <translation>Tüm Seçili &amp;Slaytlara Uygula</translation>
     </message>
 </context>
 <context>
     <name>KWpsAssistMagicOnTabbarCommand</name>
     <message>
         <source>Quick Slide Design</source>
-        <translation type="unfinished"></translation>
+        <translation>Hızlı Slayt Tasarımı</translation>
     </message>
 </context>
 <context>
     <name>KxInsertPicturesToMultiSlide</name>
     <message>
         <source>Insert Picture To Multiple Slides</source>
-        <translation type="unfinished"></translation>
+        <translation>Çoklu Slaytlara Resim Ekle</translation>
     </message>
     <message>
         <source>Unidentifiable picture format.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tanımlanamayan resim formatı.</translation>
     </message>
     <message>
         <source>The picture you inserted is too large. Please compress it to save the disk space.
 Note that compressing pictures may reduce the quality of your pictures. Do you want to compress it(them)?</source>
-        <translation type="unfinished"></translation>
+        <translation>Eklediğiniz resim çok büyük. Disk alanı kazanmak için resmi sıkıştırın.
+Unutmayın sıkıştırılan resimlerin resim kalitesi azaltabilir. Bu resim(leri) sıkıştırmak istiyor musunuz?</translation>
     </message>
     <message>
         <source>All Pictures(*.emf;*.wmf;*.jpg;*.jpeg;*.jpe;*.png;*.bmp;*.gif;*.tif;*.tiff)|*.emf;*.wmf;*.jpg;*.jpeg;*.jpe;*.png;*.bmp;*.gif;*.tif;*.tiff</source>
-        <translation type="unfinished"></translation>
+        <translation>Tüm resimler(*.emf;*.wmf;*.jpg;*.jpeg;*.jpe;*.png;*.bmp;*.gif;*.tif;*.tiff)|*.emf;*.wmf;*.jpg;*.jpeg;*.jpe;*.png;*.bmp;*.gif;*.tif;*.tiff</translation>
     </message>
     <message>
         <source>Windows Enhanced Metafile(*.emf)|*.emf</source>
         <comment>DgUil_EnhancedMetaFiles</comment>
-        <translation type="unfinished"></translation>
+        <translation>Windows Gelişmiş Meta Dosyası(*.emf)|*.emf</translation>
     </message>
     <message>
         <source>Windows Metafile(*.wmf)|*.wmf</source>
-        <translation type="unfinished"></translation>
+        <translation>Windows Meta Dosyası(*.wmf)|*.wmf</translation>
     </message>
     <message>
         <source>JPEG File InterChange Format(*.jpg;*.jpeg;*.jpe)|*.jpg;*.jpeg;*.jpe</source>
-        <translation type="unfinished"></translation>
+        <translation>JPEG Dosya Değişim Biçimi(*.jpg;*.jpeg;*.jpe)|*.jpg;*.jpeg;*.jpe</translation>
     </message>
     <message>
         <source>Portable Network Graphics(*.png)|*.png</source>
-        <translation type="unfinished"></translation>
+        <translation>Taşınabilir Ağ Grafikleri(*.png)|*.png</translation>
     </message>
     <message>
         <source>Windows Bitmap(*.bmp)|*.bmp</source>
-        <translation type="unfinished"></translation>
+        <translation>Windows Bitmap(*.bmp)|*.bmp</translation>
     </message>
     <message>
         <source>Graphics Interchange Format(*.gif)|*.gif</source>
-        <translation type="unfinished"></translation>
+        <translation>Grafik Değişim Biçimi(*.gif)|*.gif</translation>
     </message>
     <message>
         <source>Tag Image File Format(*.tif;*.tiff)|*.tif;*.tiff</source>
-        <translation type="unfinished"></translation>
+        <translation>Etiket Görüntü Dosya Biçimi(*.tif;*.tiff)|*.tif;*.tiff</translation>
     </message>
 </context>
 </TS>
