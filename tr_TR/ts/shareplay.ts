49b76d3b54f431b0f6ee60bf47d432cce3bc4c6c
Hakan <hakanseven12@gmail.com>
2016-03-04 09:57:16 +0200

    Update shareplay.ts

--- tr_TR/ts/shareplay.ts
+++ tr_TR/ts/shareplay.ts
@@ -6,90 +6,90 @@
     <name>KsSlideWindowShowMsgHandler</name>
     <message>
         <source>Scan the QR Code with wps mobile version.</source>
-        <translation type="unfinished"></translation>
+        <translation>QR Kodu WPS mobil sürümü ile tara.</translation>
     </message>
     <message>
         <source>Disconnected.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bağlantı Kesildi.</translation>
     </message>
 </context>
 <context>
     <name>KxCellPhoneControlBubble</name>
     <message>
         <source>Scan the QR Code with wps mobile version.</source>
-        <translation type="unfinished"></translation>
+        <translation>QR Kodu WPS mobil sürümü ile tara.</translation>
     </message>
     <message>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Bağlantıyı kes</translation>
     </message>
     <message>
         <source>Client connected.</source>
-        <translation type="unfinished"></translation>
+        <translation>İstemci bağlandı.</translation>
     </message>
 </context>
 <context>
     <name>KxCellPhoneControlDialog</name>
     <message>
         <source>Client Connected.</source>
-        <translation type="unfinished"></translation>
+        <translation>İstemci Bağlandı.</translation>
     </message>
 </context>
 <context>
     <name>KxCellPhoneControlWidget</name>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Vazgeç</translation>
     </message>
     <message>
         <source>Scan the QR Code with wps mobile version.</source>
-        <translation type="unfinished"></translation>
+        <translation>QR Kodu WPS mobil sürümü ile tara.</translation>
     </message>
     <message>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Bağlantıyı Kes</translation>
     </message>
     <message>
         <source>Click play button to begin.</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlamak için play düğmesine tıklayın</translation>
     </message>
     <message>
         <source>Retry</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeniden Dene</translation>
     </message>
     <message>
         <source>Network error...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ağ hatası...</translation>
     </message>
 </context>
 <context>
     <name>KxSPConfidentDlg</name>
     <message>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Bağlantıyı Kes</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Vazgeç</translation>
     </message>
     <message>
         <source>Are you sure to disconnect?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bağlantı kesilsin mi?</translation>
     </message>
 </context>
 <context>
     <name>KxSharePlayNotifyBannerWidget</name>
     <message>
         <source>Client Connected.</source>
-        <translation type="unfinished"></translation>
+        <translation>İstemci Bağlandı.</translation>
     </message>
     <message>
         <source>Client Disconnected.</source>
-        <translation type="unfinished"></translation>
+        <translation>İstemci Bağlantısı Kesildi.</translation>
     </message>
     <message>
         <source>Network error...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ağ hatası...</translation>
     </message>
 </context>
 </TS>
