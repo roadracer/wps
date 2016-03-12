901aac9accb51850e1743f61c48ec0b4290c2d1b
Hakan Seven <hakanseven12@gmail.com>
2016-03-12 18:18:22 +0200

    Update wppencoder.ts

--- tr_TR/ts/wppencoder.ts
+++ tr_TR/ts/wppencoder.ts
@@ -6,345 +6,346 @@
     <name>AudioSourceFF</name>
     <message>
         <source>FFMpeg[ASrc] - failed to allocate context for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - şunun için içerik ayrıştırma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to prepare audio stream for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - şunun için ses akışı hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to prepare frame data for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - şunun için çerçeve verisi hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to find stream information</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - akış bilgisi bulma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to find audio stream index</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - ses akışı dizini bulma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to get input audio stream</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - giriş ses akışı alma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - no appropriate decoder found: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - uygun çözücü bulunamadı: </translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to open decoder: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - çözücü açma başarısız: </translation>
     </message>
     <message>
         <source>FFMpeg[ASrc] - failed to read for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[ASrc] - şunun için okuma başarısız </translation>
     </message>
 </context>
 <context>
     <name>Encoder</name>
     <message>
         <source>Width of MediaSource must be Even</source>
-        <translation type="unfinished"></translation>
+        <translation>Olması Gereken Medya Kaynağı Genişliği</translation>
     </message>
     <message>
         <source>Height of MediaSource must be Even</source>
-        <translation type="unfinished"></translation>
+        <translation>Olması Gereken Medya Kayna Yüksekliği</translation>
     </message>
     <message>
         <source>Max size of MediaSource is </source>
-        <translation type="unfinished"></translation>
+        <translation>En Büyük Medya Kaynağı Boyutu</translation>
     </message>
 </context>
 <context>
     <name>EncoderFF</name>
     <message>
         <source>FFMpeg - failed to allocate context for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - şunun için içerik ayrıştırma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to prepare colorspace converter for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - şunun için renk alanı dönüştürücü hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to prepare video stream for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - şunun için video akışı hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to prepare audio stream for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - şunun için ses akışı hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to prepare video frame data for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - şunun için video çerçeve verisi hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to prepare audio frame data for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - şunun için ses çerçeve verisi hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to open output for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - için çıkış açılamadı</translation>
     </message>
     <message>
         <source>FFMpeg - no appropriated video codec found</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg -uygun video çözücü bulunamadı</translation>
     </message>
     <message>
         <source>FFMpeg - target format not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - hedef biçim desteklenmiyor</translation>
     </message>
     <message>
         <source>FFMpeg - no appropriate video encoder found: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - uygun video kodlayıcı bulunamadı</translation>
     </message>
     <message>
         <source>FFMpeg - failed to add video stream</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - video yayını ekleme başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - no appropriated audio codec found</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg -uygun ses çözücü bulunamadı</translation>
     </message>
     <message>
         <source>FFMpeg - no appropriate audio encoder found: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - uygun ses kodlayıcı bulunamadı:</translation>
     </message>
     <message>
         <source>FFMpeg - failed to add audio stream</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - ses yayını ekleme başarısız </translation>
     </message>
     <message>
         <source>FFMpeg - failed to fit audio channels</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - ses kanalları sığdırma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to open video encoder: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - video kodlayıcı açma başarısız:</translation>
     </message>
     <message>
         <source>FFMpeg - failed to allocate video frame for output</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - çıkış için video çerçeve ayrıştıma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to open audio encoder: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - ses kodlayıcı açma başarısız:</translation>
     </message>
     <message>
         <source>FFMpeg - failed to allocate audio frame for output</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - çıkış için ses ayrıştırma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to add inner audio of video: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - video sesi içe ekleme başarısız:</translation>
     </message>
     <message>
         <source>FFMpeg - failed to encode video frame</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - video kodlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg - failed to encode audio frame</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg - ses çerçevesi kodlama başarısız</translation>
     </message>
 </context>
 <context>
     <name>EncoderMF</name>
     <message>
         <source>MediaFoundation - need at least support of Win7 System</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - en az Win 7 sistem gerekli</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to initialize COM</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - COM ilklemesi başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to startup</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - başlangıç için başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to create writer for file: </source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - dosya için yazıcı oluşturma başarısız:</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to create input media type</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - medya türü oluşturma başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to create output media type</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - medya türü çıkışı oluşturma başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to add output media stream</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - medya yayın çıkışı ekleme başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to set InputMedia Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - medya giriş türü ayarlama başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to start Encoding</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - kodlama başlatma başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to create buffer</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - arabellek oluşturma başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to create sample</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - örnek oluşturma başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to add buffer</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - arabellek ekleme başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - I420 MediaSource is not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - I420 Medya kaynağı desteklenmiyor</translation>
     </message>
     <message>
         <source>MediaFoundation - failed to end Encoding</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - kodlama sonu başarısız</translation>
     </message>
     <message>
         <source>MediaFoundation - can&apos;t encode current shadow frame </source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu - geçerli gölge çerçevesi kodlanamadı</translation>
     </message>
     <message>
         <source>MediaFoundation - can&apos;t encode current frame </source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kuruluşu- geçerli çerçeve kodlanamadı</translation>
     </message>
 </context>
 <context>
     <name>EncoderX</name>
     <message>
         <source>x264 - failed to open encoder</source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - kodlayıcı açma başarısız</translation>
     </message>
     <message>
         <source>x264 - failed to allocate picture data</source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - resim bilgisi ayrıştırma başarısız</translation>
     </message>
     <message>
         <source>x264 - failed to allocate shadow picture data</source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - gölge resim bilgisi ayrıştırma başarısız</translation>
     </message>
     <message>
         <source>x264 - cannot open output file:</source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - çıkış dosyası açılamadı:</translation>
     </message>
     <message>
         <source>x264 - cannot set parameter of output file: </source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - çıkış dosyasını girdileri ayarlanamadı:</translation>
     </message>
     <message>
         <source>x264 - cannot close output file: </source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - çıkş dosyası kapatılamadı:</translation>
     </message>
     <message>
         <source>x264 - cannot encode current frame:</source>
-        <translation type="unfinished"></translation>
+        <translation>x264 - geçerli çerçeve kodlanamadı:</translation>
     </message>
 </context>
 <context>
     <name>KxWppEncodeCommand</name>
     <message>
         <source>an Encoding Process is already running.</source>
-        <translation type="unfinished"></translation>
+        <translation>bir kodlam işlemi zaten çalışıyor.</translation>
     </message>
     <message>
         <source>Cannot Save, </source>
-        <translation type="unfinished"></translation>
+        <translation>Kaydedilemedi,</translation>
     </message>
     <message>
         <source>the file has been opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>dosya açılmış.</translation>
     </message>
     <message>
         <source>failed to create storage for video exporting.</source>
-        <translation type="unfinished"></translation>
+        <translation>video aktarma için bellek oluşturma başarısız.</translation>
     </message>
     <message>
         <source>failed to preprocess media for video exporting.</source>
-        <translation type="unfinished"></translation>
+        <translation>video aktarma için medya önişleme başarısız.</translation>
     </message>
     <message>
         <source>Some media cannot be included.Sure to continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bazı medya içinde olamaz. Devam etmek istiyor musunuz ?</translation>
     </message>
     <message>
         <source>failed to export video for insufficient disk space. 
 Please do disk cleanup first.</source>
-        <translation type="unfinished"></translation>
+        <translation>yetersiz bellek alanı yüzünden dosya akatarımı başarısız. 
+Lütfen ilk boş bellek alanı açınız.</translation>
     </message>
 </context>
 <context>
     <name>KxWppEncodeStation</name>
     <message>
         <source>Exporting Video </source>
-        <translation type="unfinished"></translation>
+        <translation>Video Aktarılıyor</translation>
     </message>
     <message>
         <source>(Embedding Video Source)</source>
-        <translation type="unfinished"></translation>
+        <translation>(Video Kaynağı Gömme)</translation>
     </message>
     <message>
         <source>Current output size : </source>
-        <translation type="unfinished"></translation>
+        <translation>Geçerli çıkış büyüklüğü : </translation>
     </message>
     <message>
         <source>Width of MediaSource alignment is wrong.</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya kaynak genişliği sıralaması yanlış.</translation>
     </message>
     <message>
         <source>Height of MediaSource must be Even.</source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kaynağı yüksekliği eşit olmalı.</translation>
     </message>
     <message>
         <source>Max size of MediaSource is </source>
-        <translation type="unfinished"></translation>
+        <translation>Medya Kaynağı En Fazla Boyutu</translation>
     </message>
 </context>
 <context>
     <name>VideoSourceFF</name>
     <message>
         <source>FFMpeg[VSrc] - failed to allocate context for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] -  için içerik ayrıştırma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to prepare video stream for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - video akışı hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to prepare video frame data for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - video çerçeve verisi hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to prepare colorspace converter for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - frenk alanı dönüştürücü hazırlama başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to find stream info</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - akış bilgisi bulma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to find video stream index</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - akış bilgisi bulma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to get input video stream</source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - video akış girişi alma başarısız</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - no appropriate decoder found: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - uygun çözücü bulunamadı:</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to open decoder: </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - çözücü açma başarısız:</translation>
     </message>
     <message>
         <source>FFMpeg[VSrc] - failed to read for </source>
-        <translation type="unfinished"></translation>
+        <translation>FFMpeg[VSrc] - okuma başarısız</translation>
     </message>
 </context>
 </TS>
