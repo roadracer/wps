65bf703c5d539af962ec0b0f7f38be9a23f01d59
Hakan Seven <hakanseven12@gmail.com>
2016-04-03 19:47:40 +0300

    Update officespace.ts

--- tr_TR/ts/officespace.ts
+++ tr_TR/ts/officespace.ts
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="tr_TR">
-<defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>KActiveDialogAtStartup</name>
     <message>
